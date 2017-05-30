ef2374bc5e2f82e0d98ed2f018821453fdaf4da3
Edisson Reinozo <edisson350@gmail.com>
2017-05-30 01:28:41 -0500

    composite

--- BridgeMethod/src/bridgemethod/BridgeMethod.java
+++ BridgeMethod/src/bridgemethod/BridgeMethod.java
@@ -15,7 +15,12 @@ public class BridgeMethod {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Shape redCircle = new Circle(10, 10, 5, new RedCircle());
+        Shape greenCircle = new Circle(10, 10, 5, new GreenCircle());
+
+        redCircle.draw();
+        greenCircle.draw();
+
     }
-    
+
 }
